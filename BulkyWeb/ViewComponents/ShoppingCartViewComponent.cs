9de14346bbe7ccb445e206595285601b156baf31
RidayDalal <dalalriday0106@gmail.com>
2024-06-06 09:05:14 -0500

    changes

--- BulkyWeb/ViewComponents/ShoppingCartViewComponent.cs
+++ BulkyWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -1,56 +1,9 @@
-﻿/*using System.Security.Claims;
-using Bulky.DataAccess.Repository.IRepository;
-using Bulky.Utility;
-using Microsoft.AspNetCore.Mvc;
-
-namespace BulkyWeb.ViewComponents
-{
-    public class ShoppingCartViewComponent : ViewComponent
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-
-        public async Task<IViewComponentResult> InvokeAsync()
-        {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            // If the claim is not NULL, then we know that the user has already logged in.
-            // So, we retrieve the count of distict objects in the user's cart, and allow 
-            // it to get displayed on the screen.
-            if (claim != null)
-            {
-                // If the session we want to invoke has not been initialized yet, then we 
-                // initialize it.
-                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
-                {
-                    HttpContext.Session.SetInt32(SD.SessionCart,
-                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
-                }
-                
-                return View(HttpContext.Session.GetInt32(SD.SessionCart));
-            }
-            else
-            {
-                // This line clears the session of the shopping cart, i.e., once the 
-                // user logs out, the number next to the cart will reset back to zero.
-                HttpContext.Session.Clear();
-                return View(0);
-            }
-        }
-    }
-}
-*/
-
-
-using Bulky.DataAccess.Repository.IRepository;
+﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace BulkyBookWeb.ViewComponents
+namespace BulkyWeb.ViewComponents
 {
     public class ShoppingCartViewComponent : ViewComponent
     {
